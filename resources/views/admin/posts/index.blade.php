1285562a0d9c051013bbf30dcc0c7d698ead91c8
Alessandro Marinuzzi <alessandromarinuzzi@gmail.com>
2021-07-20 21:30:44 +0100

    - Copy index function from guest PostController. - Paste it into admin PostController. - Make admin post controller point at new view admin.posts.index. - Create table in admin.posts.index

---
+++ resources/views/admin/posts/index.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.admin')
+
+@section('content')
+
+<div class="table-responsive">
+    <table class="table table-hover">
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>TITLE</th>
+                <th>BODY</th>
+                <th>AUTHOR</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($posts as $post)
+            <tr>
+                <td>{{$post->id}}</td>
+                <td>{{$post->title}}</td>
+                <td>{{$post->body}}</td>
+                <td>{{$post->author}}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
+
+@endsection
