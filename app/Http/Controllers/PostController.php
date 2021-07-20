031a20f49c18829f6337169b66591216000ac4d3
Alessandro Marinuzzi <alessandromarinuzzi@gmail.com>
2021-07-20 21:18:12 +0100

    - Roughly create post-card structure guest side. - Type 'posts' in the query to check.

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -15,7 +15,7 @@ class PostController extends Controller
     public function index()
     {
         $posts = Post::all();
-        return view('guest.posts.index');
+        return view('guest.posts.index', compact('posts'));
     }
 
     /**
--- resources/views/guest/posts/index.blade.php
+++ resources/views/guest/posts/index.blade.php
@@ -2,4 +2,18 @@
 
 @section('content')
 
+<div class="container">
+    <div class="row">
+        @foreach($posts as $post)
+            <div class="col-md-4">
+                <div class="post_card">
+                    <h2 class="post_title">{{$post->title}}</h2>
+                    <p class="post_body">{{$post->body}}</p>
+                    <h5 class="post_author">{{$post->author}}</h5>
+                </div>
+            </div>
+        @endforeach
+    </div>
+</div>
+
 @endsection
