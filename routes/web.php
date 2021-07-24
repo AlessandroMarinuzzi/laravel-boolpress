2f7cdf1591ce5c53072fa589af3ae427a8a35caa
Alessandro Marinuzzi <alessandromarinuzzi@gmail.com>
2021-07-24 17:56:33 +0100

    - Implement second method to save details in db. - Create new model related routes in web.php. - Create Model, Migration and Controller.

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,16 @@ use Illuminate\Support\Facades\Route;
 // User Routes
 Route::get('/', 'PageController@index')->name('welcome');
 Route::get('about', 'PageController@about')->name('about');
+
+#Contact form
 Route::get('contacts', 'PageController@contacts')->name('contacts');
 Route::post('contacts', 'PageController@sendContactForm')->name('contacts.send');
 
+#Model related contact form
+Route::get('contacts', 'ContactController@form')->name('contacts');
+Route::post('contacts', 'ContactController@send')->name('contacts.send');
+
+#Post controller guest side.
 Route::resource('posts', PostController::class)->only('index', 'show');
 
 // Admin Routes
