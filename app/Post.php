d4c1b865f0a74c0b0e8f4f02d45cbfd787af45ac
Alessandro Marinuzzi <alessandromarinuzzi@gmail.com>
2021-07-26 15:08:37 +0100

    - Assign category to each post by tinker. - Add category_id to $fillable in Post Model.

--- app/Post.php
+++ app/Post.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    protected $fillable = ['title','image','body','author'];
+    protected $fillable = ['title','image','body','author','category_id'];
 
     public function category(){
         return $this->belongsTo(Category::class);
